e4e6f52b1f24b1b65ac53299c3ff3e325054176c
Jose Maria Vindas Ortiz <josemaria130@outlook.es>
2025-04-20 17:44:15 -0600

    READ_MEM

--- Main.cpp
+++ Main.cpp
@@ -63,11 +63,13 @@ int hex_str_to_dec_int(string hexVal)
 // Clase para el Interconect
 class Interconect{
     private: 
+        string messages;
         //Memoria Compartida, 4096 memoria total (32 bits de ancho x 128 de largo)
         // Alineada en 4 byts
         bool memoria[32][128]; 
     public:
         Interconect(){
+            messages = "";
             for (int i = 0; i < 32; ++i){
                 for (int j = 0; j < 128; ++j){
                     memoria[i][j] = 0;
@@ -77,6 +79,9 @@ class Interconect{
         // Escribe en memoria del interconect
         // Escribe de DEST(PE), en la direccion (DIR) escribe el valor (OBJ), con valor de prioridad (PRIO)
         string Write(string DEST, string DIR, string OBJ, string PRIO){
+            string temp1 = "WRITE_MEM " + DEST + ", " + DIR + " " + OBJ + ", " + PRIO + "\n";
+            KeepMessage(temp1);
+
             // Obtiene valores
             int temp0 = DIR.length();
             DIR.erase(temp0-1,1);
@@ -87,7 +92,7 @@ class Interconect{
             int C = temp0%128;
 
             // Empieza a escribir en memoria cache
-            string temp1 = OBJ;
+            temp1 = OBJ;
             for (int i = 0; i < temp1.length(); ++i){
                 if (temp1[i] == 49){
                     memoria[L][C] = 1;
@@ -103,13 +108,49 @@ class Interconect{
                 }
             }
 
-            temp1 = "WRITE_RESP ";
-            temp1 = temp1 + DEST + ", ";
-            temp1 = temp1 + "0x1, ";
-            temp1 = temp1 + PRIO;
+            temp1 = "WRITE_RESP " + DEST + ", 0x1, "+ PRIO + "\n";
+            KeepMessage(temp1);
             return temp1;
         }
-        // Imprime la memoria del Interconnect
+
+        string Read(string DEST, string DIR, string OBJ, string PRIO){
+            string temp1 = "READ_MEM " + DEST + ", " + DIR + ", " + OBJ + ", " + PRIO + "\n";
+            KeepMessage(temp1);
+            // Obtiene valores
+            DIR.erase(0,2);
+            OBJ.erase(0,2);
+            int temp0 = stoi(DIR);
+            int L = temp0/128;
+            int C = temp0%128;
+
+             // Empieza a leer en memoria cache
+            temp0 = hex_str_to_dec_int(OBJ);
+            temp1 = "";
+            for (int i = 0; i < temp0*8; ++i){
+                if (memoria[L][C] == 0){
+                    temp1.append("0");
+                 }
+                 else {
+                     temp1.append("1");
+                }
+                C++;
+                // Salta a la siguente linea en caso de overflow
+                if (C == 128){
+                   C = 0;
+                   L++;
+                }
+            }
+            temp1 = "READ_RESP " + DEST + ", " + temp1 + ", " + PRIO + "\n";
+            KeepMessage(temp1);
+            return temp1;
+        }
+
+        // Guarda los mensajes
+        void KeepMessage(string sended){
+            messages = messages + sended;
+        }
+
+        // Imprime la memoria y mensajes del Interconnect
         void Result(){
             ofstream MyFile("MemoryInterconnect.txt");
             for (int i = 0; i < 32; ++i){
@@ -127,6 +168,10 @@ class Interconect{
                 MyFile << "\n";
             }
             MyFile.close();
+
+            ofstream MyFile2("MessagesInterconnect.txt");
+            MyFile2 << messages;
+            MyFile2.close();
         }
 };
 
@@ -174,8 +219,10 @@ class PE{
                         aux0 = mystring;
                         myfile >> mystring;
                         aux1 = mystring;
-                        WriteCache(aux0,aux1);
                         KeepMessage(aux0 + " " + aux1 + "\n");
+                        aux1.erase(0,2);
+                        aux1 = hex_str_to_bin_str(aux1);
+                        WriteCache(aux0, aux1);
                     // Funcion de WRITE_MEM (ADDRESS in memory, NUMBER_OF_BYTES, ADDRESS in cache)
                     } else if (mystring == "WRITE_MEM"){
                         myfile >> mystring;
@@ -185,10 +232,18 @@ class PE{
                         myfile >> mystring;
                         aux2 = mystring;
                         KeepMessage(aux0 + " " + aux1 + " " + aux2 + "\n");
-                        aux2 = ReadCache(aux2,aux1);
+                        aux2 = ReadCache(aux2, aux1);
                         aux1 = "0x" + to_string(name);
-                        aux0 = INTERCONECT.Write(aux1,aux0,aux2,prioridad);
-                        KeepMessage(aux0 + "\n");
+                        aux0 = INTERCONECT.Write(aux1, aux0, aux2, prioridad);
+                        KeepMessage(aux0);
+                    // Funcion de WRITE_MEM (ADDRESS in memory to cache)
+                    } else if (mystring == "READ_MEM"){
+                        myfile >> mystring;
+                        KeepMessage(mystring + "\n");
+                        aux1 = "0x" + to_string(name);
+                        aux0 = INTERCONECT.Read(aux1, mystring, "0x1", prioridad);
+                        KeepMessage(aux0);
+                        //WriteCache(mystring, aux0);
                     }
                 }         
             }
@@ -227,15 +282,14 @@ class PE{
             int temp0 = DIR.length();
             DIR.erase(temp0-1,1);
             DIR.erase(0,2);
-            OBJ.erase(0,2);
             temp0 = stoi(DIR);
             int L = temp0/128;
             int C = temp0%128;
 
             // Empieza a escribir en memoria cache
-            string temp1 = hex_str_to_bin_str(OBJ);
+            string temp1 = OBJ;
             for (int i = 0; i < temp1.length(); ++i){
-                if (temp1[i] == 49){
+                if (temp1[i] == 49){ // 49 = "1" en ascci
                     memory[L][C] = 1;
                 } else
                 {
